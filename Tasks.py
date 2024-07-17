6efd2d1ee092cbbda4204bab640214b3c41fe839
Arkadii01 <118442204+Arkadii01@users.noreply.github.com>
2024-07-17 09:00:54 +0300

    Add files via upload
    Основной файл

---
+++ Tasks.py
@@ -0,0 +1,24 @@
+﻿from time import sleep
+import os
+
+def main():
+    print('Введите действие:')
+    print('1. Создание новой задачи')
+    print('2. Изменение задачи')
+    print('3. Вывод задач')
+    answer = input()
+    if answer == '1':
+        pass
+    elif answer == '2':
+        pass
+    elif answer == '3':
+        pass
+    else:
+        print('Неверный запрос!')
+        sleep(2)
+        os.system('cls')
+        main()
+
+
+if __name__ == '__main__':
+    main()
