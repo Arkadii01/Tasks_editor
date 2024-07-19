830e5fba41ffb54b249e6ca4c2aef28511ddc930
Arkadii01 <118442204+Arkadii01@users.noreply.github.com>
2024-07-19 09:49:12 +0300

    Update Tasks.py

--- Tasks.py
+++ Tasks.py
@@ -17,7 +17,7 @@ def end_db(db):
 # создание новой задачи
 def new_task():
     os.system('cls')
-    print('Выход - 0')
+    print('0 - Выход')
     name = input('Введите название задачи:')
     if name == '':
         print('Название не должно быть пустым!')
@@ -44,8 +44,13 @@ def new_task():
             else:
                 # создание задачи
                 db, c = start_db()
-                c.execute('INSERT INTO Task VALUES ()').fetchall()
+                tasks_count = int(len(c.execute('SELECT id FROM Tasks').fetchall()))
+                print(tasks_count)
+                c.execute('INSERT INTO Tasks VALUES (?, ?, ?, ?)', (tasks_count+1, name, start, end)).fetchall()
                 end_db(db)
+                print('Задача создана!')
+                sleep(2)
+                new_task()
 
 # создание имеющейся задачи
 def change_task():
@@ -55,6 +60,12 @@ def change_task():
 
 # показ задачи (диаграмма Ганта, обычный список)
 def show_task():
+    os.system('cls')
+    db, c = start_db()
+    end_db(db)
+    
+# показ задачи (диаграмма Ганта, обычный список)
+def else_time():
     os.system('cls')
     db, c = start_db()
     end_db(db)
@@ -68,6 +79,7 @@ def main():
     print('3. Вывод задач')
     print('4. Сколько осталось жить?')
     print('5. Настройки')
+    print('6. Выход')
     answer = input()
     if answer == '1':
         new_task()
@@ -78,6 +90,8 @@ def main():
     elif answer == '4':
         pass
     elif answer == '5':
+        pass
+    elif answer == '6':
         pass
     else:
         print('Неверный запрос!')
